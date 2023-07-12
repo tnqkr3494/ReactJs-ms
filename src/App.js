4afbdd2ac56d049fe87696d65cca7906d9766572
InGwonKang <tnqkr3494@naver.com>
2023-07-12 21:05:20 +0900

    props&extend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
 function App() {
   return (
-    <div>
-      <h1>welcome back!</h1>
-    </div>
+    <Father>
+      <Box bgColor="teal" />
+      <Circle bgColor="tomato" />
+    </Father>
   );
 }
 
