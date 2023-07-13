eb260e6b35308e59029e3e5285b3c737aeb961df
InGwonKang <tnqkr3494@naver.com>
2023-07-13 21:37:04 +0900

    optional props

--- reactjs-ms/src/App.tsx
+++ reactjs-ms/src/App.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import Circle from "./Circle";
 
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <Circle borderColor="yellow" bgColor="teal" />
+      <Circle text="im here" bgColor="tomato" />
+    </div>
+  );
 }
 
 export default App;
