9326bc018d788e4c3f64686f57a1bb93e0f59ec6
InGwonKang <tnqkr3494@naver.com>
2023-07-13 22:51:47 +0900

    themes

--- reactjs-ms/src/index.tsx
+++ reactjs-ms/src/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
-import App from "./App";
 import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { darkTheme, lightTheme } from "./theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
