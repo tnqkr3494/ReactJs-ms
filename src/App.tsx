9ff29eca0e24fb1dfd6eed8e4a4c303c8b33bcfe
InGwonKang <tnqkr3494@naver.com>
2023-07-14 22:51:05 +0900

    coin_home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,77 @@
-import React, { useState } from "react";
+import { createGlobalStyle } from "styled-components";
+import Router from "./Router";
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, menu, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+main, menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure,
+footer, header, hgroup, main, menu, nav, section {
+  display: block;
+}
+/* HTML5 hidden-attribute fix for newer browsers */
+*[hidden] {
+    display: none;
+}
+body {
+  line-height: 1;
+}
+menu, ol, ul {
+  list-style: none;
+}
+blockquote, q {
+  quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+* {
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Source Sans Pro', sans-serif;
+  background-color:${(props) => props.theme.bgColor};
+  color:${(props) => props.theme.textColor}
+}
+a {
+  text-decoration:none;
+  color:inherit;
+}
+`;
 
 function App() {
-  return null;
+  return (
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
+  );
 }
+// <> : 붙어 있는 컴포넌트를 만들고 싶은데 쓸모없는 div는 사용하고 싶지 않을 때 사용할 수 있는 방법(return은 하나의 덩어리만 가능)
 
 export default App;
