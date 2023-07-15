880141738e54edf84bf90e087729141789c54f24
InGwonKang <tnqkr3494@naver.com>
2023-07-15 09:53:49 +0900

    Link_to

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,11 +1,52 @@
-import { useParams } from "react-router";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+  text-align: center;
+  display: block;
+`;
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.header`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 interface RouteParams {
   coinId: string;
 }
+interface RouteState {
+  name: string;
+}
 
 function Coin() {
+  const [loading, setLoading] = useState(true);
   const { coinId } = useParams<RouteParams>();
-  return <h1>Coin: {coinId}</h1>;
+  const { state } = useLocation<RouteState>();
+  return (
+    <Container>
+      <Header>
+        <Title>{state?.name || "Loading..."}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
 }
+
+// state?.name : 우리는 link to를 이용해서 state를 보내주고 있기 때문에 만약 이 url로 바로 연결해서 사용하려고 하면
+// state값이 오지 않기 때문에 state.name값이 정의가 되지 않아 오류가 발생할 수 있다.
+
 export default Coin;
