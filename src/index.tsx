fc97a5a448691c79c5c7b0ff942a57fd6014f479
InGwonKang <tnqkr3494@naver.com>
2023-07-16 10:12:02 +0900

    themeprovider

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { ThemeProvider } from "styled-components";
 import App from "./App";
-import { theme } from "./theme";
 
 const queryClient = new QueryClient();
 
@@ -14,9 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
+      <App />
     </QueryClientProvider>
   </React.StrictMode>
 );
