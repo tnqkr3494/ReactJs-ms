22b9a59dcced8c8f404cda0d0c7d0583c1d5c396
InGwonKang <tnqkr3494@naver.com>
2023-07-19 09:21:01 +0900

    set

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,35 +1,18 @@
 import { atom, selector } from "recoil";
 
-export enum Categories {
-  "TO_DO" = "TO_DO",
-  "DOING" = "DOING",
-  "DONE" = "DONE",
-}
-/* 
-enum을 통해 "TO_DO라는 값을 0이라고 저장할 수 있다.(TO_DO라고 보이지만 컴퓨터상 0이라 저장되어있음)
-ToDo.tsx에 button값에 name에는 숫자가 아닌 문자로 들어가야 하기 때문에 TO_DO=TO_DO라고 string값으로 변환해서 사용한 것
-*/
-export interface IToDo {
-  text: string;
-  id: number;
-  category: Categories;
-}
-
-export const categoryState = atom<Categories>({
-  key: "category",
-  default: Categories.TO_DO,
+export const minuteState = atom({
+  key: "minutes",
+  default: 0,
 });
 
-export const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
-});
-
-export const toDoSelector = selector({
-  key: "toDoSelector",
+export const hourSelector = selector<number>({
+  key: "hours",
   get: ({ get }) => {
-    const toDos = get(toDoState);
-    const category = get(categoryState);
-    return toDos.filter((toDo) => toDo.category === category);
+    const minutes = get(minuteState);
+    return minutes / 60;
+  },
+  set: ({ set }, newValue) => {
+    const minutes = Number(newValue) * 60;
+    set(minuteState, minutes);
   },
 });
